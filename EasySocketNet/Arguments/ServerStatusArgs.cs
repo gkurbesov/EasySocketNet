78acf0cc87f2cc49ee939a189b9ddf022ee9fda6
Gennady <ltrg67@gmail.com>
2020-11-24 23:58:57 +0300

    server status argument

---
+++ EasySocketNet/Arguments/ServerStatusArgs.cs
@@ -0,0 +1,16 @@
+﻿using EasySocketNet.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySocketNet.Arguments
+{
+    public class ServerStatusArgs : EventArgs
+    {
+        public ServerStatusType Status { get; }
+        internal ServerStatusArgs(ServerStatusType serverStatus)
+        {
+            Status = serverStatus;
+        }
+    }
+}
