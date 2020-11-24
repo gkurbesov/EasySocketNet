14a8a314c09ce5578dc1953fb9d25353cb72b5de
Gennady <ltrg67@gmail.com>
2020-11-25 00:03:08 +0300

    client interface connected to the server

---
+++ EasySocketNet/IClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace EasySocketNet
+{
+    public interface IClient
+    {
+        int ClientId { get; }
+        EndPoint RemoteEndPoint { get; }
+    }
+}
