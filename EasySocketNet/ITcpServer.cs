5214aa7324559d51cf1528065215372069b2b38f
Gennady <ltrg67@gmail.com>
2020-11-25 00:03:42 +0300

    Tcp server interface

---
+++ EasySocketNet/ITcpServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasySocketNet.Arguments;
+using EasySocketNet.Data;
+
+namespace EasySocketNet
+{
+    public interface ITcpServer : IDisposable
+    {
+        event EventHandler<ServerStatusArgs> OnChangeStatus;
+        event EventHandler<ReceivedArgs> OnReceive;
+        event EventHandler<ConnectionArgs> OnClientConnect;
+        event EventHandler<ConnectionArgs> OnClientDisconnect;
+
+        object Tag { get; set; }
+        int Online { get; }
+        ServerStatusType Status { get; }
+        int DefaultReceiveBufferSize { get; set; }
+        int DefaultSendBufferSize { get; set; }
+
+        void Start(int port);
+        void Stop();
+        void Send(int clientId, byte[] value);
+        void Kick(int clientId);
+
+        IClient GetClient(int clientId);
+        IEnumerable<IClient> GetClients();
+    }
+}
