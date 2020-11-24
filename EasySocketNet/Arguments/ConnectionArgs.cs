8d27eeee48c4d8f5b0d76ec02d4490017b35baf7
Gennady <ltrg67@gmail.com>
2020-11-25 00:01:49 +0300

    added client id

---
+++ EasySocketNet/Arguments/ConnectionArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySocketNet.Arguments
+{
+    public class ConnectionArgs
+    {
+        public int ClientId { get; }
+
+        internal ConnectionArgs(int clientId)
+        {
+            ClientId = clientId;
+        }
+    }
+}
--- EasySocketNet/Arguments/ReceivedArgs.cs
+++ EasySocketNet/Arguments/ReceivedArgs.cs
@@ -7,11 +7,20 @@ namespace EasySocketNet.Arguments
 {
     public class ReceivedArgs : EventArgs
     {
+        public int ClientId { get; }
         public EndPoint RemoteEndPoint { get; }
         public byte[] Data { get; }
 
         internal ReceivedArgs(EndPoint endPoint, byte[] data)
         {
+            ClientId = -1;
+            RemoteEndPoint = endPoint;
+            Data = data;
+        }
+
+        internal ReceivedArgs(int clientId, EndPoint endPoint, byte[] data)
+        {
+            ClientId = clientId;
             RemoteEndPoint = endPoint;
             Data = data;
         }
